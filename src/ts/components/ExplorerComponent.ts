4b360756a52731774e63d7952d9b4fd531925c15
Lucas Reis <lucas@programmer.com.br>
2017-08-21 16:36:39 -0300

    Criar método para calcular setar fields dos arquivos
    Foi criado o método setFields para setar os fields dos arquivos que não tenham um.
    A lib es6 foi alterada para es7 para que fosse possível utilizar o método "includes" em Arrays.

--- src/ts/components/ExplorerComponent.ts
+++ src/ts/components/ExplorerComponent.ts
@@ -5,7 +5,7 @@ import { Component, Inject, Model, Prop, Watch, Provide } from 'vue-property-dec
 
 @Component({
     template: `<div id="explorer_component" style="border: 2px dashed gray" :style="{width: width + 'px', height: height + 'px'}">
-                   <ex-file v-for="file in files" :key="file.id" :file="file" :coll="getLeft(file)" :row="getTop(file)" :dragLimitSelector="dragLimitSelector"></ex-file>
+                   <ex-file v-for="file in files" :key="file.id" :file="file" :left="getLeft(file)" :top="getTop(file)" :dragLimitSelector="dragLimitSelector"></ex-file>
                </div>`,
     components: {
         "ex-file" : FileComponent
@@ -21,9 +21,10 @@ export default class ExplorerComponent extends Vue {
 	height: number;
 	
 	dragLimitSelector: string = null;
-	matriz: Array<Array<File>> = [[]];
 	maxSizeX: number = 5;
 	maxSizeY: number = 5;
+	private FILE_WIDTH: number = 110;
+	private FILE_HEIGHT: number = 140;
 	
 	constructor() {
 		super();
@@ -31,29 +32,23 @@ export default class ExplorerComponent extends Vue {
 	}
 	
 	mounted(){
-		this.loadMatriz();
+		this.setFields();
 	}
 	
-	loadMatriz() {
+	setFields() {
+		let usedFields: Array<number> = [];
+		this.files.forEach(f => {
+			if(f.field !== -1){
+				usedFields.push(f.field);
+			}		
+		});
+		let counter = -1;
 		this.files.forEach(f => {
 			if(f.field === -1){
-				loop:
-				for(let y = 0; y < this.maxSizeY; y++){
-					for(let x = 0; x < this.maxSizeX; x++){
-						if(this.matriz[y][x] === undefined){
-							this.matriz[y][x] = f;
-							break loop;
-						}
-					}
-				}
-			}else{
-			
-			}			
+				while(usedFields.includes(counter++));
+				f.field = counter;
+			}		
 		});
-	}
-	
-	getFirstFreeField() : number {
-		return null;
 	}
 	
 	addFile (file: File): void {
@@ -72,11 +67,13 @@ export default class ExplorerComponent extends Vue {
 		
 	}
 	
-	getLeft(file: File) : number{
-		return 0;
+	getLeft(file: File) : number {
+		let top = Math.trunc(file.field / this.maxSizeX);
+		return (top * this.FILE_WIDTH) + 5;
 	}
 	
-	getTop(file: File) : number{
-		return 0;
+	getTop(file: File) : number {
+		let top = Math.trunc(file.field / this.maxSizeX);
+		return (top * this.FILE_HEIGHT) + 5;
 	}
 }
