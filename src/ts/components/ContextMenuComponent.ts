7bc5ad5c46b76fe36574a17f1fabf7f3120dbc89
Lucas Reis <lucas@programmer.com.br>
2017-09-04 00:11:41 -0300

    Dont`s let  context menu overcome Explorer`s div
    Somw watchers have been added to check if the context menu is going to be created outside Explorer`s div. If yes, it will change its default position.

--- src/ts/components/ContextMenuComponent.ts
+++ src/ts/components/ContextMenuComponent.ts
@@ -1,11 +1,11 @@
 import Vue from 'vue';
-import { Component, Inject, Model, Prop, Watch, Provide } from 'vue-property-decorator';
+import { Component, Prop, Watch} from 'vue-property-decorator';
 import {Option} from '../interfaces/Option';
 import File from "../interfaces/File";
 
 @Component({
     template: `<transition name="explorer-fade">
-                    <div class="explorer-context-menu" v-show="showMenu" :style="{ top: top + 'px', left: left + 'px'}">
+                    <div class="explorer-context-menu" v-show="showMenu" :style="{ top: topPx + 'px', left: leftPx + 'px'}">
                         <div class="box">
                             <div v-if="option.visible" class="option" :class="{disabled: option.disabled}" v-for="option in options" @click.stop="callback($event, option)">
                                 <a href="javascript:void(0)">{{option.name}}</a>
@@ -32,6 +32,8 @@ export default class ContextMenuComponent extends Vue {
     file: File;
 
     showMenu: boolean = false;
+    leftPx: number = 0;
+    topPx: number = 0;
 	
 	@Watch('show')
 	onShowChanged(val: boolean, oldVal: boolean) {
@@ -42,9 +44,32 @@ export default class ContextMenuComponent extends Vue {
     onShowMenuChange(val: boolean, oldVal: boolean) {
         this.$emit('update:show', val)
     }
+
+    @Watch('left')
+    onLeftChange(val: number, oldVal: number) {
+	    const explorerWidth: number = Number(document.getElementById('explorer-component').style.width.replace('px', ''));
+        const cmWidth = Number(this.$el.getBoundingClientRect().width);
+	    if(explorerWidth - val < cmWidth){
+	        this.leftPx = val - cmWidth;
+        }else{
+            this.leftPx = val;
+        }
+    }
+
+    @Watch('top')
+    onTopChange(val: number, oldVal: number) {
+        const explorerHeight: number = Number(document.getElementById('explorer-component').style.height.replace('px', ''));
+        const cmHeight = this.$el.getBoundingClientRect().height;
+        if(explorerHeight - val < cmHeight){
+            this.topPx = val - cmHeight;
+        }else{
+            this.topPx = val;
+        }
+    }
 	
 	mounted(){
 		this.registerListeners();
+		this.leftPx = this.left;
 	}
 	
 	callback(e, option: Option){
@@ -56,10 +81,6 @@ export default class ContextMenuComponent extends Vue {
 	
 	registerListeners() {
 	    const that = this;
-		document.addEventListener("click", (e) => {
-            that.showMenu = false;
-		});
-		
 		document.addEventListener("closeAllContextMenu", (e) => {
             that.showMenu = false;
 		});
